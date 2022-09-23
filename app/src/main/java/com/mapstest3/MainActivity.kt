82f0937c5f9696c5129484a8d72f2363666e811d
cfallasg@quanticaits.com <cfallasg@quanticaits.com>
2022-09-24 00:42:22 +0200

    Initial commit

---
+++ app/src/main/java/com/mapstest3/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.mapstest3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker() {
+        val favoritePlace = LatLng(10.005975, -84.215964)
+        map.addMarker(MarkerOptions().position(favoritePlace).title("Mi playa favorita!"))
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(favoritePlace, 18f),
+            4000,
+            null
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createMapFragment()
+    }
+
+    private fun createMapFragment() {
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+}
